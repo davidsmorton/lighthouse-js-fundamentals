3ce7b7ef72d319978f37345925b9a98f683d8b7a
davidsmorton <davidsmorton@gmail.com>
2020-08-28 16:26:23 +0000

    Array Operations and Properties task

---
+++ Array_Ops_Prop.js
@@ -0,0 +1,13 @@
+const director = "Steven Spielbert";
+const films = ["Jaws", "Raiders of the Lost Ark", "A.I"];
+console.log(`My favourite ${director} film is ${films[1]}.`);
+console.log(`films at index 0: ${films[0]}`);
+console.log(`films at index 1: ${films[1]}`);
+console.log(`films at index 2: ${films[2]}`);
+console.log(`films at index 3: ${films[3]}`);
+films[2] = "Jurassic Park";
+films.push("A.I");
+console.log(films);
+console.log(`${director} is the director of ${films.length} films`);
+films.push("E.T.");
+console.log(`${director} is the director of ${films.length} films`);
